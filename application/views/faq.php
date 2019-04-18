0c2adcce931619b07982e4910d3754592c2ecb7c
joshgal <joga3333.jg@gmail.com>
2019-04-18 21:54:18 +0700

    add views(about, faq, privacy, term)
    need to be tidy up

---
+++ application/views/faq.php
@@ -0,0 +1,169 @@
+<?php $this->load->view('page_header')?>
+<div style="position: relative; padding: 120px 40px 0px; color: rgb(74, 74, 74);" class="css-nil FAQ-body container">
+	<div class="columns is-mobile">
+		<div class="column is-12-mobile is-8-tablet is-9-desktop">
+			<div class="css-1k13f9w for-starter">
+				<h1 id="1" class="css-kouncy">For Starters</h1>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">1. What is KROWD?</div>
+				<div class="css-au8nn4">KROWD is an online platform that aims to bridge the collaboration between Indonesian creators who want to produce creations that are not only inspiring, but also impactful. A platform where designers, animators, comic artists, musicians, writers, artists, crafters, and app developers can meet and produce high-quality masterpieces that can go global. With KROWD, you will be able to make your works into something bigger, together.</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">2. Why should I join KROWD?</div>
+				<div class="css-au8nn4">By joining KROWD, you can either initiate a Project you want to build, or participate in other Initiator’s Project to contribute with your expertise.
+					<div class="css-f3khu"></div>If you initiate a Project, you can recruit Collaborator whose interest and expertise suits your need to develop your own Project.
+					<div class="css-f3khu"></div>On the other hand, if you participate in other Initiator’s Project, not only you will get a fun experience, you will also get a Reward from the Initiator: credits, gifts, or even profit sharing!
+				</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">3. How much does it cost to join KROWD?</div>
+				<div class="css-au8nn4">We don’t charge any fee for Initiators and Collaborators to join our platform. All it takes is your commitment to collaborate that can bring impact.</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">4. How can I register as KROWD’s member?</div>
+				<div class="css-au8nn4">To be a member of KROWD, you can register yourself first 
+					<a href="/signup" class="css-8clr06">here</a>.
+					<div class="css-f3khu">
+						
+					</div>Already have an account? Log in 
+					<a href="/login" class="css-8clr06">here</a>.
+				</div>
+				<div class="css-nuwf1p"></div>
+				<h1 id="1" class="css-kouncy">Initiators</h1>
+				<div class="css-qx91dp"></div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">1. What is an Initiator?</div>
+				<div class="css-au8nn4">Initiator is a person, organization, or an institution who initiate a Project. Initiator will be responsible to run the Project and select people to collaborate with them to run their Project.
+					<a href="/login" class="css-8clr06">here</a>.
+				</div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">2. Who can be an Initiator?</div>
+				<div class="css-au8nn4">Everyone who has a Project to initiate and need Collaborator to join their Project, can be n Initiator. For more information about Initiator, go to the Initiator Guideline.</div>
+				<div class="css-nuwf1p"></div>
+				<h1 id="1" class="css-kouncy">Collaboratos</h1>
+				<div class="css-qx91dp"></div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">1. What is a Collaborator?</div>
+				<div class="css-au8nn4">Collaborator is a person who will collaborate with the Initiator to run Project. Collaborator can nominate themselves to join Projects that suits their interest and expertise.</div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">2. Who can be a Collaborator?</div>
+				<div class="css-au8nn4">Everyone who have skills and can commit to a Project, can be a Collaborator. For more information about Collaborator, go to the Collaborator Guideline.</div>
+				<div class="css-nuwf1p"></div>
+				<h1 id="1" class="css-kouncy">Projects</h1>
+				<div class="css-qx91dp"></div>
+				<div class="css-nuwf1p"></div><div id="d" class="css-19d2vu5">1. What is a Project?</div>
+				<div class="css-au8nn4">Project is an event or work that is initiated by Initiator, where Collaborator and Initiator can work together to run it.</div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">2. What are the projects’ categories?</div>
+				<div class="css-au8nn4">At KROWD, we have 8 main categories of Projects. Those categories are:</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Art &amp; Culture</div>
+				<div class="css-au8nn4">
+				<div class="css-f3khu"></div>Visual art, fine art, dance, culture, performance, and any other stuff that relate to this field are included in Art &amp; Culture category.</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Fashion</div>
+				<div class="css-au8nn4">
+				<div class="css-f3khu"></div>Projects in this section are those that related to clothing, makeup, footwear, and accessories. This section also includes fashion runway, exhibition and many more.</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Film &amp; Video</div>
+				<div class="css-au8nn4"><div class="css-f3khu"></div>Film production, videography, script writing, movies, and other things that aim to use moving images to convey messages.
+				</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Games</div>
+				<div class="css-au8nn4">
+				<div class="css-f3khu"></div>Games development, games design, and other Projects that related to building a game.</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Music</div>
+				<div class="css-au8nn4">
+				<div class="css-f3khu">
+									
+				</div>From singing to song writing, from music composing to organizing concert, are included in this category.</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Publishing</div>
+				<div class="css-au8nn4">
+				<div class="css-f3khu"></div>Journalism, copywriting, comic, new literature, printed media, and event podcasts Projects are included in Publishing category.</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Social</div>
+				<div class="css-au8nn4">
+				<div class="css-f3khu"></div>Non-profit projects for social cause that has impact for society, in areas such as education, environment, energy, health, and welfare.</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Technology</div>
+				<div class="css-au8nn4">
+				<div class="css-f3khu"></div>From programming to Artificial Intelligence (AI), from Internet of Things (IOT) to digital startup.</div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">3. How to initiate a Project?</div>
+				<div class="css-au8nn4">At Project page, you will have a chance to tell the whole world the answers of questions above. Here are 5 points that you can use to help you explain your Project:</div>
+				<div class="css-nuwf1p"></div>
+				<div class="css-19d2vu5">Who are you?</div>
+				<div class="css-19d2vu5">What are you going to make?</div>
+				<div class="css-19d2vu5">Where this Project comes from?</div>
+				<div class="css-19d2vu5">What is your plan for this Project?</div>
+				<div class="css-19d2vu5">When will you run this Project?</div>
+				<div class="css-19d2vu5">Why should this Project happened?</div>
+				<div class="css-nuwf1p">
+					
+				</div>
+				<h1 id="1" class="css-kouncy">Others</h1>
+				<div class="css-qx91dp"></div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">1. How to edit my profile?</div>
+				<div class="css-au8nn4">To edit and change the information that you want to show on your profile page in KROWD, you can click your profile picture icon on the top right of your page → choose Settings → choose Edit Profile.
+				</div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">2. Can I access KROWD through mobile app?</div>
+				<div class="css-au8nn4">Yes, you can. We already launched the first version of KROWD app in the Google Play Store. It’s free and you can download it 
+					<a href="https://play.google.com/store/apps/details?id=id.krowd.krowd" class="css-8clr06">here
+					</a>. In our app, you can only access KROWD to join a Project, edit your profile, and manage your Project.
+				</div>
+				<div class="css-nuwf1p"></div>
+				<div id="d" class="css-19d2vu5">3. What is KROWD’s policy regarding the use of KROWD?</div>
+				<div class="css-au8nn4">For policy, terms, and rules in KROWD, please check the Terms and Conditions.</div> class="css-nuwf1p"></div>
+				<div class="css-au8nn4">If you have more questions that isn’t listed in this FAQ, feel free to contact us for further info</div>
+			</div>
+		</div>
+		<div class="column is-hidden-mobile is-4-tablet is-3-desktop">FAQ
+			<ul class="ant-tree" role="tree-node" unselectable="on">
+				<li class="">
+					<span class="ant-tree-switcher ant-tree-switcher_open"></span>
+						<span title="For Starters" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-open">
+							<span class="ant-tree-title">For Starters</span>
+						</span>
+						<ul class="ant-tree-child-tree ant-tree-child-tree-open" data-expanded="true">
+							<li class="">
+								<span class="ant-tree-switcher ant-tree-switcher-noop"></span>
+								<span title="What is KROWD?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal">
+									<span class="ant-tree-title">What is KROWD?</span>
+								</span>
+							</li>
+							<li class="">
+								<span class="ant-tree-switcher ant-tree-switcher-noop"></span>
+								<span title="Why should I join KROWD" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal">
+									<span class="ant-tree-title">Why should I join KROWD</span>
+								</span>
+							</li>
+							<li class="">
+								<span class="ant-tree-switcher ant-tree-switcher-noop"></span>
+								<span title="How much does it cost to join KROWD?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal ant-tree-node-selected">
+									<span class="ant-tree-title">How much does it cost to join KROWD?</span>
+								</span>
+							</li>
+							<li class="">
+								<span class="ant-tree-switcher ant-tree-switcher-noop"></span>
+								<span title="How can I register as KROWD’s member?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal">
+									<span class="ant-tree-title">How can I register as KROWD’s member?</span>
+								</span>
+							</li>
+						</ul>
+					</li>
+					<li class="">
+						<span class="ant-tree-switcher ant-tree-switcher_open"></span>
+						<span title="Initiators" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-open">
+							<span class="ant-tree-title">Initiators</span>
+						</span>
+						<ul class="ant-tree-child-tree ant-tree-child-tree-open" data-expanded="true">
+							<li class="">
+								<span class="ant-tree-switcher ant-tree-switcher-noop"></span>
+								<span title="What is an Initiator?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal">
+									<span class="ant-tree-title">What is an Initiator?</span>
+								</span>
+							</li>
+							<li class="">
+								<span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="Who can be an Initiator?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">Who can be an Initiator?</span></span></li></ul></li><li class=""><span class="ant-tree-switcher ant-tree-switcher_open"></span><span title="Collaborators" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-open"><span class="ant-tree-title">Collaborators</span></span><ul class="ant-tree-child-tree ant-tree-child-tree-open" data-expanded="true"><li class=""><span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="What is an Collaborators?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">What is an Collaborators?</span></span></li><li class=""><span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="Who can be an Collaborators?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">Who can be an Collaborators?</span></span></li></ul></li><li class=""><span class="ant-tree-switcher ant-tree-switcher_open"></span><span title="Projects" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-open"><span class="ant-tree-title">Projects</span></span><ul class="ant-tree-child-tree ant-tree-child-tree-open" data-expanded="true"><li class=""><span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="What is a Project?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">What is a Project?</span></span></li><li class=""><span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="Who are projects' categories?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">Who are projects' categories?</span></span></li><li class=""><span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="How to initiate a project" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">How to initiate a project</span></span></li></ul></li><li class=""><span class="ant-tree-switcher ant-tree-switcher_open"></span><span title="Others" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-open"><span class="ant-tree-title">Others</span></span><ul class="ant-tree-child-tree ant-tree-child-tree-open" data-expanded="true"><li class=""><span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="How to edit my profile?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">How to edit my profile?</span></span></li><li class=""><span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="Can I access KROWD through mobile app?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">Can I access KROWD through mobile app?</span></span></li><li class=""><span class="ant-tree-switcher ant-tree-switcher-noop"></span><span title="What is KROWD’s policy regarding the use of KROWD?" class="ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal"><span class="ant-tree-title">What is KROWD’s policy regarding the use of KROWD?</span></span></li></ul></li></ul></div></div></div>
+<?php $this->load->view('page_footer')?>								
