bb35d7487f7b83d5d344d939b28305291f6b5eca
Revaldi Gilang Azhari <revaldigilangazhari@gmail.com>
2019-04-19 20:30:56 +0700

    Login & Registrasi
    Menambahkan MVC Login & Registrasi

---
+++ application/controllers/Auth.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('Auth_model');
+    }
+    public function index(){
+        $this->load->view('page_header');
+        $this->load->view('welcome_message');
+        $this->load->view('page_footer');
+       
+    }
+    public function login(){
+        $this->form_validation->set_rules('username','Username','required|trim');
+        $this->form_validation->set_rules('password','Password','required|trim');
+        if($this->form_validation->run() != TRUE){ 
+            $this->load->view('page_header');
+            $this->load->view('auth/Login');
+            $this->load->view('page_footer');
+        }else{
+            $this->Auth_model->login();
+        }
+    
+    }
+  
+    public function registrasi(){
+        $this->form_validation->set_rules('username','Username','required|trim');
+        $this->form_validation->set_rules('email','Email','required|trim|valid_email');
+        $this->form_validation->set_rules('password','Password','required|trim|min_length[3]|matches[re_password]');
+        $this->form_validation->set_rules('re_password','Re_password','required|trim|min_length[3]|matches[password]');
+
+        if($this->form_validation->run() != TRUE){
+            $this->load->view('page_header');
+            $this->load->view('auth/registrasi');
+            $this->load->view('page_footer');
+        }else{
+		
+             $this->Auth_model->registrasi();
+               redirect('auth/Login');		
+            }
+
+        
+    }
+   
+}
---
+++ application/models/Auth_model.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth_model extends CI_Model {
+    public function login(){
+        $username = $this->input->post('username');
+        $password =  $this->input->post('password');
+        $cek = $this->db->get_where('user',['username' => $username])->row_array();
+        if($cek){
+            //jika username ada
+            if(password_verify($password, $cek['password'])){
+                $data = [
+                    'username' => $cek['username'],
+                    'email' => $cek['email']
+                ];
+                    $this->session->set_userdata($data);
+                    redirect('user');
+            }else{
+                $this->session->set_flashdata('message','Gk ada');
+            }
+
+        }else{
+            $this->session->set_flashdata('message','Gk ada');
+            redirect('auth/login');
+        }
+    }
+    public function registrasi(){
+    $data = [
+        "username" => $this->input->post('username', true),
+        "email" => $this->input->post('email', true),
+        "password" => password_hash($this->input->post('password', true), PASSWORD_DEFAULT)
+    ];
+
+    return $this->db->insert('user', $data);
+}
+}
