6225c0de619f87e662fba0042a4505342249b69a
joshgal <joga3333.jg@gmail.com>
2019-04-22 22:03:28 +0700

    Ngerapihin Views

--- application/views/page_footer.php
+++ application/views/page_footer.php
@@ -3,12 +3,12 @@
       <div class="container">
           <div class="row">
               <ul class="nav d-flex justify-content-left mr-auto ">
-                <li class="nav-item"> <a class="nav-link" id="ft-item" href="<?= base_url('c_krowd_home/term')?>">Term &Condition</a> </li>
-                <li class="nav-item"> <a class="nav-link" id="ft-item" href="<?= base_url('c_krowd_home/faq')?>">FAQ</a> </li>
-                <li class="nav-item"> <a class="nav-link" id="ft-item" href="<?= base_url('c_krowd_home/privacy')?>">Privacy Policy</a> </li>
+                <li class="nav-item"> <a class="nav-link linkFooter" id="ft-item" href="<?= base_url('c_krowd_home/term')?>">Term & Condition</a> </li>
+                <li class="nav-item"> <a class="nav-link linkFooter2" id="ft-item" href="<?= base_url('c_krowd_home/faq')?>">FAQ</a> </li>
+                <li class="nav-item"> <a class="nav-link linkFooter2" id="ft-item" href="<?= base_url('c_krowd_home/privacy')?>">Privacy Policy</a> </li>
               </ul>
               <ul class="navbar-nav">
-                <p>Copyright © 2019 KROWD.ID</p>
+                <p class="linkFooter3">Copyright © 2019 KROWD.ID</p>
               </ul>
           </div>
       </div>
