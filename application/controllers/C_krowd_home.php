89a0a970171b792fd40e964d5efc94cf8b5a28f3
joshgal <joga3333.jg@gmail.com>
2019-04-26 10:05:31 +0700

    Revert "add admin"
    This reverts commit 9d0672ac7ebe042eaf619865fb13eb2b8f51ec32.

--- application/controllers/C_krowd_home.php
+++ application/controllers/C_krowd_home.php
@@ -42,7 +42,6 @@ class C_krowd_home extends CI_Controller {
 	{
 		$data['project'] = $this->krowd_model->get_all_ChProjects_brief();
 		$data['nproject'] = $this->krowd_model->get_all_notChProjects_brief();
-		$data['cproject'] = $this->krowd_model->get_all_closeProjects_brief();
 		$this->load->view('admin_menu', $data);
 	}
 
@@ -51,22 +50,4 @@ class C_krowd_home extends CI_Controller {
 		$this->krowd_model->update_project_admin_check($id);
 		redirect('c_krowd_home/admin');
 	}
-
-	public function close_stat($id)
-	{
-		$this->krowd_model->close_project_admin_check($id);
-		redirect('c_krowd_home/admin');
-	}
-	public function restore_stat($id)
-	{
-		$this->krowd_model->restore_project_admin_check($id);
-		redirect('c_krowd_home/admin');
-	}
-	public function add_queue($iduser, $idproject)
-	{
-		$this->krowd_model->adding_collaborator_to_queue($iduser, $idproject);
-		redirect('c_krowd_home/index');
-	}
-
-	
 }
